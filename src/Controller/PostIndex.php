6e87b1b05de519509611063c70aa9eb7dc789db7
Emmanuelle Mercadal <mercadal.em@gmail.com>
2022-11-25 18:17:16 -0700

    Display all posts

--- src/Controller/PostIndex.php
+++ src/Controller/PostIndex.php
@@ -3,6 +3,8 @@
 namespace silverorange\DevTest\Controller;
 
 use silverorange\DevTest\Context;
+use silverorange\DevTest\Model\Author;
+use silverorange\DevTest\Model\Post;
 use silverorange\DevTest\Template;
 
 class PostIndex extends Controller
@@ -18,12 +20,40 @@ class PostIndex extends Controller
 
     public function getTemplate(): Template\Template
     {
-        return new Template\PostIndex();
+        return new Template\PostIndex($this->posts);
     }
 
     protected function loadData(): void
     {
-        // TODO: Load posts from database here.
-        $this->posts = [];
+        $postRequest = $this->db->query('
+            SELECT p.id AS post_id, p.title AS post_title, p.body AS post_body,
+                    p.created_at AS post_created_at, p.modified_at AS post_modified_at,
+                    a.id AS author_id, a.full_name AS author_full_name, a.created_at AS author_created_at,
+                    a.modified_at AS author_modified_at
+            FROM posts p
+            JOIN authors a ON p.author = a.id
+            ORDER BY p.created_at DESC
+        ');
+        $data = $postRequest->fetchAll();
+
+        foreach ($data as $row) {
+            // TODO: duplicated part, would need refactoring
+            $author = new Author(
+                $row['author_id'],
+                $row['author_full_name'],
+                $row['author_created_at'],
+                $row['author_modified_at']
+            );
+            $post = new Post(
+                $row['post_id'],
+                $row['post_title'],
+                $row['post_body'],
+                $row['post_created_at'],
+                $row['post_modified_at'],
+                $author
+            );
+
+            $this->posts[] = $post;
+        }
     }
 }
--- src/Template/PostIndex.php
+++ src/Template/PostIndex.php
@@ -6,12 +6,43 @@ use silverorange\DevTest\Context;
 
 class PostIndex extends Layout
 {
+    private array $posts = [];
+
+    public function __construct(array $posts)
+    {
+        parent::__construct();
+        $this->posts = $posts;
+    }
+
     protected function renderPage(Context $context): string
     {
         // @codingStandardsIgnoreStart
-        return <<<HTML
-<p>SHOW ALL THE POSTS HERE</p>
+        $render = <<<HTML
+            <h1 class="frame__title">Posts list</h1>
 HTML;
+
+        if (empty($this->posts)) {
+            $render .= <<<HTML
+                <div>
+                    No article has been posted yet. Stayned tuned <i class="fa-regular fa-face-smile"></i>
+                </div>
+HTML;
+        } else {
+            foreach ($this->posts as $post) {
+                $render .= <<<HTML
+                    <div class="frame">
+                        <h2 class="frame__title">{$post->title}</h2>
+                        <div class="frame__contents">
+                            <div>Publié par {$post->author->full_name}</div>
+                            <hr>
+                            <a href="/posts/{$post->id}">See post</a>
+                        </div>
+                    </div>
+HTML;
+            }
+        }
+
         // @codingStandardsIgnoreEnd
+        return $render;
     }
 }
